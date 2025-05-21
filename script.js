223b62e4e2be4bff2113ee0d2f4960d1aa7e72b1
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-21 19:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // write js code if required
+let flip=document.querySelector(".flip");
+let img=document.querySelector("img");
+img.addEventListener("onmouseover",()=>{
+	img.src="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+	
+})
